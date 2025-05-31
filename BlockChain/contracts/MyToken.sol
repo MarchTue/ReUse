36c944a9d15fe6a9331d1bc4fbe7cab66d61b065
YooByWk <tls19190@naver.com>
2025-05-31 20:16:49 +0900

    [BC]Feat #5 : Add AccessControl
    - Switched from `Ownable` to `AccessControl`.
    - `ADMIN_ROLE` now controls minting and other admin tasks.
    - Deployer is automatically granted `ADMIN_ROLE`.
    - `addAdmin` function allows existing admins to add new ones.
    - `DEFAULT_ADMIN_ROLE` is managed by `ADMIN_ROLE`.

--- BlockChain/contracts/MyToken.sol
+++ BlockChain/contracts/MyToken.sol
@@ -2,7 +2,10 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+// import "@openzeppelin/contracts/utils/Context.sol";
 
 /**
  * @title MyToken
@@ -10,39 +13,62 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  * 원화(KRW)와 1:1 매칭을 위한 decimals : 0으로 설정
  * 컨트랙트의 소유자만이 추가 토큰을 발행 할 수 있습니다.
  */
-contract MyToken is ERC20, Ownable {
-  event Minted(address indexed to, uint256 amount);
-  event Burned(address indexed from, uint256 amount);
+contract MyToken is ERC20, AccessControl {
+    event Minted(address indexed to, uint256 amount);
+    event Burned(address indexed from, uint256 amount);
 
-  
-  constructor (string memory name, string memory symbol, uint256 initialSupply) 
-  ERC20(name, symbol) 
-  Ownable(msg.sender)
-  {
-    _mint(msg.sender, initialSupply);
-    emit Minted(msg.sender, initialSupply);
-  }
-  
-  function decimals() public view virtual override returns (uint8) {
-    return 0; // 1 원 = 1 토큰
-  }
-  
-  function mint(address to, uint256 amount) public onlyOwner {
-    require(to != address(0), "ERC20: mint to the zero address");
-    require(amount > 0, "ERC20: mint amount must be greater than 0");
-    
-    _mint(to, amount);
-    
-    emit Minted(to, amount);
-  }
-  
-  function burn(uint256 amount) public {
-    require(amount > 0, "ERC20: burn amount must be greater than 0");
-    require(balanceOf(msg.sender) >= amount, "ERC20: burn amount exceeds balance");
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
-    _burn(msg.sender, amount);
-    emit Burned(msg.sender, amount);
-  }
+    modifier onlyOwner() {
+        require(
+            hasRole(ADMIN_ROLE, _msgSender()),
+            "MyToken: caller is not the owner"
+        );
+        _;
+    }
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply
+    )
+        // address[] memory admins
+        ERC20(name, symbol)
+    {
+        _grantRole(ADMIN_ROLE, _msgSender());
+        _setRoleAdmin(DEFAULT_ADMIN_ROLE, ADMIN_ROLE);
+        _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _mint(msg.sender, initialSupply);
+        emit Minted(msg.sender, initialSupply);
+        decimals();
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 0; // 1 원 = 1 토큰
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(ADMIN_ROLE) {
+        require(to != address(0), "ERC20: mint to the zero address");
+        require(amount > 0, "ERC20: mint amount must be greater than 0");
+
+        _mint(to, amount);
+
+        emit Minted(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        require(amount > 0, "ERC20: burn amount must be greater than 0");
+        require(
+            balanceOf(msg.sender) >= amount,
+            "ERC20: burn amount exceeds balance"
+        );
+
+        _burn(msg.sender, amount);
+        emit Burned(msg.sender, amount);
+    }
+
+    function addAdmin(address account) public onlyOwner {
+        require(account != address(0), "ERC20: add admin to the zero address");
+        grantRole(ADMIN_ROLE, account);
+    }
 }
-
-
--- BlockChain/ignition/modules/token.test.module.ts
+++ BlockChain/ignition/modules/token.test.module.ts
@@ -1,8 +1,7 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
 
 export default buildModule("MyToken", (m) => {
-  const token = m.contract("MyToken", ["ERC20", "KIWI", 10]);
-  m.call(token, "decimals", []);
-  
+  const token = m.contract("MyToken", ["ERC20", "KIWI", 31]);
+  // m.call(token, "decimals", []);
   return ({ token });
-}); 
+});

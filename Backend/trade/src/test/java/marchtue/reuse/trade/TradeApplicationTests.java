14b6725f0d2f08d647c3aeccd5b5535eb1415f17
le-monaaa <kgbn013@naver.com>
2025-06-23 21:47:47 +0900

    [BE]Feat #27: 거래 서비스 기초 구현
    - Init

---
+++ Backend/trade/src/test/java/marchtue/reuse/trade/TradeApplicationTests.java
@@ -0,0 +1,13 @@
+package marchtue.reuse.trade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

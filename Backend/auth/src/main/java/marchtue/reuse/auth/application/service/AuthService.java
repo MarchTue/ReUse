368e6e16d62a341e98facc850d33902f9e22371b
le-monaaa <kgbn013@naver.com>
2025-06-30 15:25:20 +0900

    [BE]Feat #24: role 추가
    - 토큰 정보에 role 추가
    - trade-build.gradle 수정(jwt)

--- Backend/auth/src/main/java/marchtue/reuse/auth/application/service/AuthService.java
+++ Backend/auth/src/main/java/marchtue/reuse/auth/application/service/AuthService.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import marchtue.reuse.auth.application.client.UserClient;
 import marchtue.reuse.auth.application.dto.request.UserInfoRequest;
 import marchtue.reuse.auth.application.dto.response.UserInfoResponse;
+import marchtue.reuse.auth.domain.enums.UserRoleEnum;
 import marchtue.reuse.auth.domain.model.UserSession;
 import marchtue.reuse.auth.domain.repository.UserSessionRepository;
 import marchtue.reuse.auth.global.ApiResponse;
@@ -27,21 +28,18 @@ public class AuthService {
   private final UserSessionRepository sessionRepository;
   private final JwtUtil jwtUtil;
 
-
   public ApiResponse certify(UserInfoRequest req, HttpServletResponse response) {
     UserInfoResponse user = userClient.findByCi(req.ciHs());
 
-    // 신규 이용자
     if (user.userId() == null) {
       return new ApiResponse(404, "new user", "");
     }
 
-    // 기존 이용자
     userClient.DidCheckDid(user.userId(), req.did());
-    String accessToken = jwtUtil.createAccessToken(user.userId());
+
+    String accessToken = jwtUtil.createAccessToken(user.userId(), user.role());
     String refreshToken = jwtUtil.createRefreshToken(user.userId());
 
-    // 세션 저장
     UserSession session = UserSession.builder()
         .deviceId(req.deviceId())
         .userAgent(req.userAgent())
@@ -55,6 +53,7 @@ public class AuthService {
 
     HttpHeaders headers = jwtUtil.createAccessTokenHeader(accessToken);
     ResponseCookie refreshCookie = jwtUtil.createRefreshTokenCookie(refreshToken);
+
     response.addHeader(HttpHeaders.SET_COOKIE, refreshCookie.toString());
     response.addHeader(JwtUtil.AUTHORIZATION_HEADER,
         headers.getFirst(JwtUtil.AUTHORIZATION_HEADER));
@@ -62,7 +61,6 @@ public class AuthService {
     return new ApiResponse<>(200, "logined", List.of(Map.of("user_id", user.userId())));
   }
 
-  // 토큰 재발급
   public ApiResponse reissue(HttpServletRequest request, HttpServletResponse response) {
     String refreshToken = jwtUtil.getTokenFromCookie(request, jwtUtil.REFRESH_TOKEN_COOKIE);
 
@@ -73,19 +71,15 @@ public class AuthService {
     Claims claims = jwtUtil.getUserInfoFromToken(refreshToken);
     String username = claims.getSubject();
 
-    String newAccessToken = jwtUtil.createAccessToken(username);
-//    String newRefreshToken = jwtUtil.createRefreshToken(username);
+    String roleStr = claims.get("role", String.class);
+    UserRoleEnum role = UserRoleEnum.valueOf(roleStr);
+
+    String newAccessToken = jwtUtil.createAccessToken(username, role);
 
     HttpHeaders accessHeader = jwtUtil.createAccessTokenHeader(newAccessToken);
     response.addHeader(jwtUtil.AUTHORIZATION_HEADER,
         accessHeader.getFirst(jwtUtil.AUTHORIZATION_HEADER));
-//    ResponseCookie refreshCookie = jwtUtil.createRefreshTokenCookie(newRefreshToken);
-//    response.addHeader(HttpHeaders.SET_COOKIE, refreshCookie.toString());
 
     return new ApiResponse(200, "token issued", null);
-
   }
-
-
 }
-

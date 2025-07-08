e713c66985b230f2d62a69169fb31c05300e4a6e
YooByWk <tls19190@naver.com>
2025-07-09 02:03:16 +0900

    [FE] Feat #41: 홈페이지 구조 변경 및 서버 데이터 함수 주석 추가

--- reuse-app/src/app/home/page.tsx
+++ reuse-app/src/app/home/page.tsx
@@ -1,8 +1,24 @@
+/*
+// 서버에서 데이터를 가져올 함수
+async function getFavouriteCategoryItems() {
+  
+}
+// 
+async function getRecentItems() {
+  
+}
+*/
 
+import { AppHeader } from "@/components/layout/app-header";
 
+export default async function Home() {
+  // 추후 가져올 아이템...
 
-export default function Home() {
   return (
-    <>홈</>
+    <div className="min-h-screen bg-white">
+      <AppHeader
+      />
+    </div>
+
   );
 }

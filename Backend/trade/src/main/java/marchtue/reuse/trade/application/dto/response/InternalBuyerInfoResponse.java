cf877f4ccf9dc0dd7cb699e50c1c767cfab5567c
le-monaaa <kgbn013@naver.com>
2025-07-21 14:01:40 +0900

    [BE]Feat #40: 게시글 상세 조회
    - 누락 레코드 추가

---
+++ Backend/trade/src/main/java/marchtue/reuse/trade/application/dto/response/InternalBuyerInfoResponse.java
@@ -0,0 +1,14 @@
+package marchtue.reuse.trade.application.dto.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import java.util.UUID;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record InternalBuyerInfoResponse(
+    UUID userId,
+    String nickname,
+    String profile
+) {
+
+}

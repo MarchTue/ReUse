9014b2928c2ec3109a425bad9de421c96b2223a0
YooByWk <tls19190@naver.com>
2025-08-14 19:46:28 +0900

    [BC]Feat #71: Enhance Eth module with WebSocketProvider integration and logging

--- BlockChain/monitor/src/eth/eth.module.ts
+++ BlockChain/monitor/src/eth/eth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { Eth } from './eth';
-import { Eth } from './eth';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  providers: [Eth]
+  providers: [Eth, ConfigService],
+  exports: [Eth]
 })
-export class EthModule {}
+export class EthModule { }

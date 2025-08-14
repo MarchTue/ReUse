81cf1e6e5c13c9c6be8ca40ddf374bb37056b5c1
YooByWk <tls19190@naver.com>
2025-08-14 18:54:13 +0900

    [BC]Setup #71: add Prisma config

--- BlockChain/monitor/src/app.module.ts
+++ BlockChain/monitor/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventWatcherModule } from './event-watcher/event-watcher.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [EventWatcherModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }

7c087137e33b14e4d427eb84bd2c7914ff205ace
YooByWk <tls19190@naver.com>
2025-09-02 22:11:49 +0900

    [FE]Feat #78: 추천 상품 컴포넌트에 카테고리 prop 추가 및 mockItems 사용

--- reuse-app/src/components/home/recommend.tsx
+++ reuse-app/src/components/home/recommend.tsx
@@ -1,14 +1,20 @@
 "use client";
+import { mockItems } from "@/lib/mock/mock-items";
 import ItemTitleButton from "../item/item-title-button";
+import Image from 'next/image';
+import ItemList from "../item/item-list";
 
 // 해당 카테고리를 불러와서 넣는거니까, 서버측 렌더도 될 듯
 // X -> 해당 유저의 카테고리이므로, client
-export default function Recommend() {
-  const title = "추천 상품";
+export default function Recommend({ category = "추천 상품" }: { category?: string; }) {
+  // const title = "추천 상품";
+  const recommendedItems = mockItems.sort(() => 0.5 - Math.random()).slice(0, 5);
+
   return (
     <>
-      <div>recommend</div>
-      <ItemTitleButton title={title} />
+      {/* <div>recommend</div> */}
+      {/* <ItemTitleButton title={title} /> */}
+      <ItemList title={category} items={recommendedItems} categoryId="category ID" loading={false} />
     </>
   );
 }

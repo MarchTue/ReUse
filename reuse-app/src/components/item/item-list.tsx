44ed7f0e57dbd5b6f467e37c2bb4904b56f45014
YooByWk <tls19190@naver.com>
2025-09-03 11:45:46 +0900

    [FE]Feat #78: 아이템 리스트에서 카드 컴포넌트를 링크로 감싸기

--- reuse-app/src/components/item/item-list.tsx
+++ reuse-app/src/components/item/item-list.tsx
@@ -2,6 +2,7 @@ import { IItem } from "@/types/item";
 import ItemTitleButton from "./item-title-button";
 import Image from "next/image";
 import { Card, CardContent } from '@/components/ui/card';
+import Link from "next/link";
 
 
 interface IItemList {
@@ -30,31 +31,36 @@ export default function ItemList({
       ) : (
         <div className="">
           {items.map((item) => (
-            <Card
+            <Link
               key={item.id}
-              className="
+              href={`/item/${item.id}`} // slug 활용
+            >
+              <Card
+                key={item.id}
+                className="
               border rounded-lg p-2 hover:shadow-lg transition 
               my-4 pt-4 ml-0 pl-0
               "
-            >
-              <CardContent className="flex items-center justify-between pl-4">
-                <div className="relative w-2/5 min-h-[6rem] mb-2 rounded-md pl-0 ml-0">
-                  <Image
-                    src="/mock-item.png" alt={item.title} fill={true}
-                    className="w-full"
-                  />
-                </div>
-                <div className="flex-1 flex justify-center items-start flex-col pl-6">
-                  <p className="text-lg font-semibold">{item.title}</p>
-                  <p className="flex gap-2 text-lg font-semibold"> <Image src="/coin.png" width={24} height={18} alt="" />{item.price}</p>
-                  {/* tqh 좋아요 여부 */}
-                  {/* tqh 상품 상태 */}
-                  {/* tqh 작성시간 - dayjs */}
-                  <p className="text-ios-gray text-sm">{`${'0' + (tmpDate.getMonth() + 1)}/${tmpDate.getDate()} ${tmpDate.getHours()}:${tmpDate.getMinutes()}`}</p>
-                </div>
+              >
+                <CardContent className="flex items-center justify-between pl-4">
+                  <div className="relative w-2/5 min-h-[6rem] mb-2 rounded-md pl-0 ml-0">
+                    <Image
+                      src="/mock-item.png" alt={item.title} fill={true}
+                      className="w-full"
+                    />
+                  </div>
+                  <div className="flex-1 flex justify-center items-start flex-col pl-6">
+                    <p className="text-lg font-semibold">{item.title}</p>
+                    <p className="flex gap-2 text-lg font-semibold"> <Image src="/coin.png" width={24} height={18} alt="" />{item.price}</p>
+                    {/* tqh 좋아요 여부 */}
+                    {/* tqh 상품 상태 */}
+                    {/* tqh 작성시간 - dayjs */}
+                    <p className="text-ios-gray text-sm">{`${'0' + (tmpDate.getMonth() + 1)}/${tmpDate.getDate()} ${tmpDate.getHours()}:${tmpDate.getMinutes()}`}</p>
+                  </div>
 
-              </CardContent>
-            </Card>
+                </CardContent>
+              </Card>
+            </Link>
           ))}
         </div>
       )}

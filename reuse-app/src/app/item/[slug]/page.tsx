40a124bfa7ccd15d20fe68a2ccb2e0ddffc86bb9
YooByWk <tls19190@naver.com>
2025-09-05 21:53:04 +0900

    [FE]Feat #78: 상품 상세 페이지의 이미지 컴포넌트 개선 및 이미지 슬라이더 기능 추가

--- reuse-app/src/app/item/[slug]/page.tsx
+++ reuse-app/src/app/item/[slug]/page.tsx
@@ -1,24 +1,30 @@
 import ProductDetail from "@/components/product/productDetail";
 import ProductHeader from "@/components/product/productHeader";
+import ProductImages from "@/components/product/productImages";
+import { mockProductDetails } from "@/lib/mock/mock-item-details";
 import { mockProducts } from "@/lib/mock/mock-items";
 import { notFound } from "next/navigation";
 
 
 
-export default async function ItemDetailPage({
+export default async function ProductDetailPage({
   params,
 }: {
   params: { slug: string; };
 }) {
-  const product = mockProducts.find((i) => i.id === params.slug);
+  // const product = mockProducts.find((i) => i.id === params.slug);
+  const productTotal = mockProductDetails.find((i) => i.product.id === params.slug);
+
+  if (!productTotal) return <div className="p-6">상품을 찾을 수 없습니다.</div>;
+
+  const product = productTotal.product;
   console.log(product);
-  if (!product) return <div className="p-6">상품을 찾을 수 없습니다.</div>;
 
 
   return (
     <div className="min-h-screen bg-white">
       <ProductHeader title={product.title} />
-      {/* <ItemImages product={product} /> */}
+      <ProductImages title={product.title} images={product.images} views={product.views} />
       <ProductDetail />
     </div>
   );
--- reuse-app/src/components/product/productImages.tsx
+++ reuse-app/src/components/product/productImages.tsx
@@ -1,24 +1,69 @@
 "use client";
 import { IProduct, IProductDetail } from "@/types/product";
 import { useState } from "react";
+import Image from 'next/image';
+import { ChevronLeft, ChevronRight, Eye } from "lucide-react";
+import { Button } from '@/components/ui/button';
 
 
 interface ProductImagesProps {
-  product: IProductDetail;
-  views: number;
+  title: string;
+  images: string[];
+  views?: number;
   className?: string;
 }
-export default function ProductImages({ product, className, views }: ProductImagesProps) {
-
-  const [currentImage, setCurrentImage] = useState(0);
+export default function ProductImages({ title, images, className, views }: ProductImagesProps) {
 
 
 
-  const nextImage = () => { };
+  const [currentImageIdx, setCurrentImageIdx] = useState(0);
+  console.log(images.length);
+  const nextImage = () => setCurrentImageIdx((prev) => (prev + 1) % images.length);
+  const prevImage = () => setCurrentImageIdx((prev) => (prev - 1 + images.length) % images.length);
 
   return (
-    <div className={className}>
-      <p>{product.product.id}</p>
-    </div>
+    <div className={`relative ${className} pt-20 flex justify-center`}>
+      <div className="aspect-square bg-gray-100 relative overflow-hidden w-[95%] h-full object-cover">
+        <Image src={images[currentImageIdx]} fill={true} alt={`title ${currentImageIdx}`} />
+
+        {images.length > 1 && (
+          <>
+            <Button
+              variant="ghost"
+              size="sm"
+              onClick={prevImage}
+              className="absolute left-4 top-1/2 -translate-y-2 w-10 h-10 rounded-full bg-black/20 text-white hover:bg-black:40"
+            >
+              <ChevronLeft className="w-6 h-6" />
+            </Button>
+
+            <Button
+              variant="ghost"
+              size="sm"
+              onClick={nextImage}
+              className="absolute right-4 top-1/2 -translate-y-2 w-10 h-10 rounded-full bg-black/20 text-white hover:bg-black:40"
+            >
+              <ChevronRight className="w-6 h-6" />
+            </Button>
+          </>
+        )}
+
+        {/* 현재 이미지 idx */}
+        <div className="absolute bottom-4 left-[45%] flex space-x-2">
+          {images.map((_, idx) => (
+            <div
+              key={idx}
+              className={`w-2 h-2 rounded-full transition-all ${idx === currentImageIdx ? "bg-white" : "bg-white/50"
+                }`}
+            />
+          ))}
+        </div>
+        <div className="absolute bottom-4 right-4 bg-black/50 text-white px-3 py-1 rounded-full text-sm flex items-center">
+          <Eye className="w-4 h-4 mr-1" />
+          {views}
+        </div>
+        {/* image div ends */}
+      </div>
+    </div >
   );
 };
